83b275c788309c40684438e673a7fcd12f1f10a5
legend3 <zhenguo50@126.com>
2021-10-07 03:22:09 +0800

    设置父类打包方式为pom

--- HelloWorldTime/src/main/java/SayHelloWorldTime.java
+++ HelloWorldTime/src/main/java/SayHelloWorldTime.java
@@ -1,6 +1,8 @@
+
+
 public class SayHelloWorldTime {
 	public String sayHWTime(String time) {//morning
-		HelloWorld hw = new HelloWorld();
+		HelloWorld hw = new HelloWorld();//依赖项目HelloWorld2中的类型！
 		return hw.sayHello("zs") +","+time;//"Hello zs,morning"
 	}
 }
--- HelloWorldTime/src/test/java/TestSayHelloWorld.java
+++ HelloWorldTime/src/test/java/TestSayHelloWorld.java
@@ -1,9 +1,9 @@
-import org.junit.Test;
+import org.junit.Test;//依赖HelloWorld2项目打包中的test
 
 import static org.junit.Assert.assertEquals;
 
 public class TestSayHelloWorld {
-	
+
 	@Test
 	public void testSayHWTime() {
 		SayHelloWorldTime shw = new SayHelloWorldTime();
